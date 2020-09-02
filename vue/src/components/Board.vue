cd07fd2463e14a8d5c0d9effb1673038fe7c6309
Daniel Kamar <kamarster@gmail.com>
2020-09-03 01:56:27 +0300

    the vue game structure and logic

---
+++ vue/src/components/Board.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="board">
+        <div>
+
+            <div class="row">
+                <Square @click.native="() => onSquareClick(0)" :value="currentBoard[0]" />
+                <Square @click.native="() => onSquareClick(1)" :value="currentBoard[1]" />
+                <Square @click.native="() => onSquareClick(2)" :value="currentBoard[2]" />
+            </div>
+
+            <div class="row">
+                <Square @click.native="() => onSquareClick(3)" :value="currentBoard[3]" />
+                <Square @click.native="() => onSquareClick(4)" :value="currentBoard[4]" />
+                <Square @click.native="() => onSquareClick(5)" :value="currentBoard[5]" />
+            </div>
+
+            <div class="row">
+                <Square @click.native="() => onSquareClick(6)" :value="currentBoard[6]" />
+                <Square @click.native="() => onSquareClick(7)" :value="currentBoard[7]" />
+                <Square @click.native="() => onSquareClick(8)" :value="currentBoard[8]" />
+            </div>
+
+        </div>
+        
+    </div>
+</template>
+
+<script>
+
+import Square from '@/components/Square.vue';
+
+export default {
+    name: 'Board',
+    components: {
+        Square,
+    },
+    props: {
+        currentBoard: Array
+    },
+    methods: {
+        onSquareClick(x) {
+            this.$emit("clickedSquare", x)
+            console('clicked', x)
+        }
+    }
+}
+</script>
+
+<style>
+    .row::after {
+        clear: both;
+        content: "";
+        display: table;
+    }
+
+</style>
---
+++ vue/src/containers/Game.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="game">
+
+      <!-- Board -->
+      <Board @clickedSquare="handleClick" :currentBoard="currentBoard"/>
+
+    <div class="game-info">
+      
+      <div>{{status}}</div>
+      <div>{{currentBoard}}</div>
+
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+
+import Board from '@/components/Board.vue';
+
+import {
+  calculateWinner, currentMove, initialState, move,
+} from '@/containers/helpers';
+
+export default {
+  name: 'Game',
+  components: {
+    Board
+  },
+  data() {
+    return {
+      state: initialState(),
+    };
+  },
+  methods: {
+    handleClick(id) {
+      const newState = move(id, this.state) 
+      if (newState) {
+        this.state = newState
+      }
+    }
+  },
+  computed: {
+    currentBoard() {
+      return currentMove(this.state).squares;
+    },
+
+    status() { // Notera: computed methods accessas utan invokering "()", dvs bara "status" inte "status()"
+      const winner = calculateWinner(this.currentBoard)
+      if (winner) {
+        return `Winner is ${winner}`
+      }
+      return `Next player: ${this.state.xIsNext ? 'X': 'O'}`; // eller "Winner is: X/O", eller överkurs: "It's a draw!"
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .game {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .game-info {
+    margin-left: 20px;
+  }
+
+
+</style>
